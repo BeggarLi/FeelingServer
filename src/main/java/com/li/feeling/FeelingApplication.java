dee487908a8c6413f92cb17a08711fdef1cba771
BeggarLi <1559067870@qq.com>
2021-09-09 23:18:37 +0800

    project creation

---
+++ src/main/java/com/li/feeling/FeelingApplication.java
@@ -0,0 +1,13 @@
+package com.li.feeling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeelingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/li/feeling/FeelingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.li.feeling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
