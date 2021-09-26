2282b58014349757b20dd6138e9dc2c87fabdb15
BeggarLi <1559067870@qq.com>
2021-09-27 00:35:08 +0800

    User类field加上序列化名字

--- src/main/java/com/li/feeling/model/User.java
+++ src/main/java/com/li/feeling/model/User.java
@@ -9,10 +9,15 @@ import java.io.Serializable;
  */
 public class User implements Serializable {
 
+    @JsonProperty("id")
     public String mId;
+    @JsonProperty("phone")
     public String mPhone; // 手机号
+    @JsonProperty("password")
     public String mPassword; // 密码
+    @JsonProperty("nickname")
     public String mNickName; // 昵称
+    @JsonProperty("sex")
     public String mSex; // 性别,M代表男，F代表女
 
     public User(String phone, String mPassword) {
